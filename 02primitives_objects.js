d1945e52b1fb44d9a28601853c061ec27c4957cf
SMD OWAIS <111645920+SMD111-git@users.noreply.github.com>
2023-12-23 14:39:30 +0000

    container config done

---
+++ 02primitives_objects.js
@@ -0,0 +1,19 @@
+// primivite data  type
+let a =null;
+let b =56;
+let c = true;
+let d = BigInt("588") + BigInt("3")
+let f = Symbol("i am ggod ")
+let g
+console.log(a,b,c,d,f,g)
+// object in js non primivite dt
+const item = {
+ /*key*/ //value
+  "ll": true,
+  "pop":false,
+  "smd":67,
+  "hkgn":undefined,
+   "dkb":null
+}
+console.log(item["hkgn"])
+console.log(item["smd"])
