1b10b83a3dd2aa781f06e5deb4816a68152b173f
SMD OWAIS <111645920+SMD111-git@users.noreply.github.com>
2023-12-24 12:53:38 +0000

    container config done

---
+++ 03operators.js
@@ -0,0 +1,12 @@
+let x=10;
+let y=20;
+let z=x+y;
+console.log(z)
+let c=x*y;
+console.log(c)
+const d=x-y;
+console.log(d)
+const e=x/y;
+console.log(e)
+let v = (10 + 5) * 3; // x is 45
+console.log(v)
