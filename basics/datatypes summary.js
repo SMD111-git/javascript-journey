82164534509af7b04e0401edacde8dd28203c4d5
SMD OWAIS <111645920+SMD111-git@users.noreply.github.com>
2024-07-09 08:16:34 +0000

    datatypes summary

---
+++ basics/datatypes summary.js
@@ -0,0 +1,31 @@
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const id = Symbol('123')
+const anotherid = Symbol('123')
+console.log(id===anotherid)
+console.log(typeof id)
+console.log(typeof anotherid)
+//both are having the same value in the varable but the both the type are not same
+
+const bigNumber=258974110326569752 //this automatically used as bigint 
+const bigNumbr=258974110326569752n //as at rear we added the 'n' it get into big int
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+ const heros =["n450","baba420","pogo"] ///this array
+ // this  object
+let myobj = {  /* this a varaible to store a object */
+    name:"SMD",//this are  objects, 
+    Age:20,
+ }
+console.log(myobj);
+
+
+//function
+const myfunction =function(){
+    console.log("helloworld");
+}
