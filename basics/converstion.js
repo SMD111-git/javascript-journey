7ca6e9e6871c048b7ce6741081085a8d485c1eeb
SMD OWAIS <111645920+SMD111-git@users.noreply.github.com>
2024-07-09 02:31:06 +0000

    convertion

--- basics/converstion.js
+++ basics/converstion.js
@@ -1,4 +1,39 @@
-let socre="33" //convering string into number
+let socre="33abbc" //convering string into number  this alpnumber
 let valueintoint = Number(socre)
 console.log(typeof valueintoint);
 console.log(typeof (valueintoint));
+console.log(valueintoint);//if ther value is not able to be converted then it gives output as "NaN"(which is an datatype)
+//wt if we pass null in this 
+let score=null //convering string into number
+let value = Number(score)
+console.log(typeof value);
+console.log(typeof (value));
+console.log(value);
+//number
+let scre="33" //convering string into number
+let intoint = Number(scre)
+console.log(typeof intoint);
+console.log(typeof (intoint));
+console.log(intoint);
+//undefined
+let cre=undefined //convering string into number
+let b = Number(cre)
+console.log(typeof b);
+console.log(typeof (b));
+console.log(b);
+//boolena value true or flase
+let a=true //convering string into number
+let c = Number(a)
+console.log(typeof c);
+console.log(typeof (c));
+console.log(c);
+//converting into string 
+let d = 890;
+let e =String(d)
+console.log(typeof e)
+console.log(e)
+//converting into boolean
+let loggedin= "";//in this true is passed when 1 is the input, where as if we pass 0 or string("","potr") then it is flase, if pass " " this then it ture
+let booleanin = Boolean(loggedin)
+console.log(typeof booleanin)
+console.log(booleanin)
