4c8b2fed7876286067109739ee4a18e7e3905824
SMD OWAIS <111645920+SMD111-git@users.noreply.github.com>
2024-07-10 11:30:38 +0000

    date.js

--- basics/date.js
+++ basics/date.js
@@ -6,11 +6,24 @@ console.log(typeof mydate) //it a datatype of no primivite and a object */
 
 
 let myCreatedDate = new Date("01-07-2024")
-console.log(myCreatedDate.toLocaleDateString());
+//console.log(myCreatedDate.toLocaleDateString());
 //console.log(myCreatedDate.toDateString());
-console.log(myCreatedDate.toString());
+//console.log(myCreatedDate.toString());
 // let myCreatedDate = new Date(2023, 0, 23, 5, 3)
 // let myCreatedDate = new Date("2023-01-14")
 
 
-let mytimestamps=Date.now() //
+/*let mytimestamps=Date.now() //
+console.log(mytimestamps);
+console.log(myCreatedDate.getTime());
+console.log(Math.round(myCreatedDate.getTime()/1000));*/
+
+let newDate = new Date()
+//console.log(newDate.getMonth()+1);
+//console.log(newDate.getDay());
+ 
+
+newDate.toLocaleString('default'),{
+    weekday:"long"
+}
+console.log(newDate)//output:2024-07-10T11:29:13.369Z
