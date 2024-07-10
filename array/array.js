b39fb19e24b354c09152414e0edc77140630b7a9
SMD OWAIS <111645920+SMD111-git@users.noreply.github.com>
2024-07-10 16:46:07 +0000

    array..js

---
+++ array/array.js
@@ -0,0 +1,16 @@
+//array
+const myarr=[1,2,3,5,6];
+const  arrman=["power","money"];
+const arr = new Array(1,2,3,4,5,6);
+console.log(myarr[0])
+
+//array methods
+myarr.push(7);
+console.log(myarr);
+myarr.pop(7);
+console.log(myarr);
+myarr.unshift(9);
+console.log(myarr)//[ 9, 1, 2, 3, 5, 6 ]The unshift() method adds new elements to the beginning of an array.
+myarr.shift();
+console.log(myarr)//[ 1, 2, 3, 5, 6 ] the shift method removes the element from the array at the beginning .
+const 
