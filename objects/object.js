82dbdeb6381773bdd633841d9abad507436504e7
SMD OWAIS <111645920+SMD111-git@users.noreply.github.com>
2024-09-29 06:30:54 +0000

    objects.js

--- objects/object.js
+++ objects/object.js
@@ -1,10 +1,12 @@
 //this a conster method or sinngleton
 //Object.create
 //object literals
-const jsuser={name:"SMD",age:"18",islogged:"true",out:"24/7"}
+const jsuser={name:"SMD",age:"18","islogged":"true",out:"24/7"}
 //in objects we can acess by key:value, we acess by keys to get the value of the object,
 console.log(jsuser.out)
 console.log(jsuser["out"])//this another method of acessing the value from object
 //to add the key:value to object
 jsuser.location="mumbai";
 console.log(jsuser);
+//if the key is in string format then to acess that 
+console.log(jsuser["islogged"]);
