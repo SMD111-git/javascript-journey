0825696f14f4382c778bf8424429c539868075c5
SMD OWAIS <111645920+SMD111-git@users.noreply.github.com>
2024-10-01 13:23:41 +0000

    function.js

--- funnction/function.js
+++ funnction/function.js
@@ -12,8 +12,8 @@ function addtwonumbers(num1,num2){
 const result=addtwonumbers(3,5);
 console.log("result:",result);//the point note down is that inn above function method the result was unndefined now it hold the value and the type 
 //other point the key word return is used as a print statment to give the output and if any thing is given  the statemnt of return then stament will not give output or excute , bcz it below the return statment
-function loginusermsg(username){
-    if(username==="s"){
+function loginusermsg(username="smd"){
+    if(!username){
         console.log("pls enter  a username")
     }
     return `${username} just logged in`
