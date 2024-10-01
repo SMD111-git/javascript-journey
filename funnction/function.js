7febbb602f9866012d390c064218cbb0f4d9d4dd
SMD OWAIS <111645920+SMD111-git@users.noreply.github.com>
2024-10-01 12:52:12 +0000

    function.js

--- funnction/function.js
+++ funnction/function.js
@@ -2,4 +2,13 @@
 function addtwonumbers(num1,num2){
     return num1+num2;
 }
-console.log(addtwonumbers(3,2));
+//console.log(addtwonumbers(3,2));
+//const result=addtwonumbers(5,3)
+//console.log(result);
+function addtwonumbers(num1,num2){
+    let result=num1+num2;
+    return result
+}
+const result=addtwonumbers(3,5);
+console.log("result:",result);//the point note down is that inn above function method the result was unndefined now it hold the value and the type 
+//other point the key word return is used as a print statment to give the output and if any thing is given  the statemnt of return then stament will not give output or excute , bcz it below the return statment
