b01267e0ac8e17a4bfafe4ce32e04391ed4e92f9
SMD OWAIS <111645920+SMD111-git@users.noreply.github.com>
2024-10-03 06:19:37 +0000

    object looping

--- looping/1.js
+++ looping/1.js
@@ -13,3 +13,26 @@ const arr=[1,2,3,4,5]
  map.set("usa","LA")
  map.set("Tk","istanbul")
  console.log(map);//output:Map(3) { 'In' => 'india', 'usa' => 'LA', 'Tk' => 'istanbul' }
+
+//for(const[key,value] of map){
+  // console.log(key,":",value);
+   
+//}//is looping method 
+//map method syntax: arr.map(function(element, index, array){  }, this);
+let ar = [2, 3, 5, 7]//looping method
+
+ar.map(function(element, index, array){
+    console.log(element);
+    console.log(index);
+    console.log(array);
+    return element;
+}, 80);//element gives u the curent value and index gives the position of the value
+
+//object lopping
+const bject={
+   game1:"basket",
+   game2:"karfton"
+}
+for (const[key,value] of bject){
+   console.log(key,":-",value);
+}
