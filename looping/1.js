ab7572d94d3f503e89a53737802a54580e858d84
SMD OWAIS <111645920+SMD111-git@users.noreply.github.com>
2024-10-03 05:33:11 +0000

    looping

---
+++ looping/1.js
@@ -0,0 +1,11 @@
+const arr=[1,2,3,4,5]
+ for (const num of  arr){
+    console.log(num);
+ }
+ //this gives yoou the the loop of array 
+ const greeting="SMD here "
+ for(const i of greeting){
+    console.log(i)
+ }//the iteratot throught each and every value.and gives it in printed output indivaly
+ //maps in array
+ 
