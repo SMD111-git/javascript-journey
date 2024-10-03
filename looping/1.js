330b9b39ded896cfb6d8034ec55f69902b0bf722
SMD OWAIS <111645920+SMD111-git@users.noreply.github.com>
2024-10-03 05:47:04 +0000

    map

--- looping/1.js
+++ looping/1.js
@@ -7,5 +7,10 @@ const arr=[1,2,3,4,5]
  for(const i of greeting){
     console.log(i)
  }//the iteratot throught each and every value.and gives it in printed output indivaly
- //maps in array
+ //maps in array and creaation of map
+ const map = new Map()
+ map.set("In","india")
+ map.set("usa","LA")
+ map.set("Tk","istanbul")
+ console.log(map);//output:Map(3) { 'In' => 'india', 'usa' => 'LA', 'Tk' => 'istanbul' }
  
