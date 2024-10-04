034ca8d646cdbcf2f4effbb3c4e95648b4b1bc3b
SMD OWAIS <111645920+SMD111-git@users.noreply.github.com>
2024-10-04 15:18:15 +0000

    loop.js

--- looping/2.js
+++ looping/2.js
@@ -5,3 +5,8 @@ const myobject={
 for(const key in myobject){
     console.log(`${key} the shortcut is ${myobject[key]}`)
 }
+
+const progarmming=["js","rb","py","java","cpp"]
+for(const key in progarmming){
+    console.log(progarmming[key])
+}
