80c913c8634051efb75c2c8d53e2a6555cfc7f18
SMD OWAIS <111645920+SMD111-git@users.noreply.github.com>
2024-10-05 17:07:40 +0000

    whileloops

--- looping/whileloop.js
+++ looping/whileloop.js
@@ -4,3 +4,22 @@ while (index<=10) {
     index=index+3;
     
 }
+//it much silimar to the for loop but the structure different 
+
+let myarray=["bat","super","asw","wosum"]
+let arr=0;
+while(arr<myarray.length){
+    console.log(`the value of array is:${myarray[arr]}`);
+    arr++;
+}//the point is that if we pass the myarray[arr] then gives the  single output element at a time
+/**
+ the value of array  is:bat
+the value of array  is:super
+the value of array  is:asw
+the value of array  is:wosum
+*/
+//but if we donot pass any anything in the myarray just give it the the output will  be 
+/*the value of array  is:bat,super,asw,wosum
+the value of array  is:bat,super,asw,wosum
+the value of array  is:bat,super,asw,wosum
+the value of array  is:bat,super,asw,wosum*/
