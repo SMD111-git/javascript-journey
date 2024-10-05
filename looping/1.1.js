57d4ce634b77a9a9499cb44d296270e7c5d22115
SMD OWAIS <111645920+SMD111-git@users.noreply.github.com>
2024-10-05 15:58:24 +0000

    for looping

---
+++ looping/1.1.js
@@ -0,0 +1,6 @@
+//for loop in this we have inisaltion,condition,increment:
+//now the loop work in a way where it check the inistal value and the then goes to condtion loop  and then to //code anfter the code then i goes back to increment and then to condition and then code
+for(let index=0; index < 10; index++) {
+    const element= index;
+    console.log(element)
+}
