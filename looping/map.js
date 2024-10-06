24b8aa8408448bca94b40a4181ec84e67e414286
SMD OWAIS <111645920+SMD111-git@users.noreply.github.com>
2024-10-06 12:00:18 +0000

    map,reduce

--- looping/map.js
+++ looping/map.js
@@ -2,3 +2,12 @@
 const mynum=[1,2,3,4,5,6,7,8,9,10]
 const newnum=mynum.map((i)=>i+10)
 console.log(newnum);
+//chaing where we have maps and filter in seques and give the final array as per the  chaning sequenes
+let mynumbers=[1,2,3,4,5,6,7,8,9,10]
+let newnumbers = mynumbers
+    .map((num)=> num*10)
+    .map((num)=>num+1)
+    .filter((num)=> num>=40)
+console.log(newnumbers);
+
+
