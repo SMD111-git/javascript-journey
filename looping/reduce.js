f60236adbd515187ddfc30f04ca50401421dae94
SMD OWAIS <111645920+SMD111-git@users.noreply.github.com>
2024-10-06 12:20:34 +0000

    reduce

--- looping/reduce.js
+++ looping/reduce.js
@@ -5,3 +5,22 @@ const mytotal=mynum.reduce(function(acc,currval) {
 },0) //pass any number eg:3
 /**this last nummber mentiod in last is inital number assigned to accvalue from where   shoulkkd the number start like where should  it go to add nummbers*/
 console.log(mytotal)
+//this zare used in shoppingcarts
+const shoppingcart=[
+    {
+        itemname:"js",
+        price:999
+    },
+    {
+        itemname:"reactjs",
+        price:999
+    },
+    {
+        itemname:"reactnative",
+        price:12000
+    },
+]
+const pricetopay=shoppingcart.reduce((acc,item)=>acc + item.price,0)
+console.log(pricetopay)
+
+//as we see item.price is add to acc and the goes on and give you the total prices value.
