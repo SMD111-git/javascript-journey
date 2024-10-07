3cfddfe0be3384ba915716ba4abf3849044df137
SMD OWAIS <111645920+SMD111-git@users.noreply.github.com>
2024-10-07 17:00:11 +0000

    dom

--- dom/dom.js
+++ dom/dom.js
@@ -1,4 +1,13 @@
 //the ponts to note down and dom(doucment object manupalation)
 /**doucment.getElementById('title')
+ * the method get you  attriube by the id and gives you the whole tag values.
+ * document.getElementById("title").id   this gives you the value  of that tag:title,it is a way to  acess the attributes of that tag.
+ * document.getElementById("title").className  silmiar here we are acess the value of that tag by  giving specifi attriube need to be displayed.
+ * to access the all attriues in that partciular tag by using getattribute()
+ * document.getElementById('title').getAttribute('id')
+ * document.getElementById('title').getAttribute('class')// this give you the values of the attriubte
+ * document.getElementById('title').setAttribute('class','test') this set or replaces the existing attribute value to the vaue which we are setting the value of that attribute.
+ * and the note is that we need pass in parameters as (attriute,value). 
+ * eg:document.getElementById('title').setAttribute('class','SMD')
  * 
  */
