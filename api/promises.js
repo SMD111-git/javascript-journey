563d730158eb5897c29f96c4e929c8319eec4bbe
SMD OWAIS <111645920+SMD111-git@users.noreply.github.com>
2024-11-12 13:17:06 +0000

    api

--- api/promises.js
+++ api/promises.js
@@ -1,13 +1,14 @@
 //creation of promises
-const promiseone = new promise(function (reslove,reject) {
+const promiseone = new promise(function (resolve,reject) {
     //DB calls,network
     //do an async task
     setTimeout(function(){
         console.log("async task is completed");
-        
+        resolve()
     },1000)
 })
-promiseone.then(function(){
+//here we are acessing the promise thing or consuming 
+promiseone.then(function(){ //over here the then() is directly  realted to the resolve argument or method 
     console.log("promise consumed");
     
 })
