05d679aa0fc5d3739894eeed4b7fdf492924fe98
SMD OWAIS <111645920+SMD111-git@users.noreply.github.com>
2024-11-12 13:14:23 +0000

    api

--- api/promises.js
+++ api/promises.js
@@ -1,1 +1,13 @@
-fetch('https:// ')
+//creation of promises
+const promiseone = new promise(function (reslove,reject) {
+    //DB calls,network
+    //do an async task
+    setTimeout(function(){
+        console.log("async task is completed");
+        
+    },1000)
+})
+promiseone.then(function(){
+    console.log("promise consumed");
+    
+})
