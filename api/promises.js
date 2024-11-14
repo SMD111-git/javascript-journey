adb5eaea3aa92c3b2b7ddc4e23a4ef4093b2a36f
SMD OWAIS <111645920+SMD111-git@users.noreply.github.com>
2024-11-14 10:11:45 +0000

    promise

--- api/promises.js
+++ api/promises.js
@@ -108,5 +108,21 @@ consumerpromisefive()
 
 
 //
+/*async function getalluser() {
+    try{
+    const response=await fetch("https://jsonplaceholder.typicode.com/users")
+    const data =await response.json()
+    console.log(data)
+}catch(error){
+    console.log("E:",error);
+    
+}
+}
+getalluser()*/
 
+fetch('https://api.github.com/users/SMD111-git').then (function(respons){
+    return respons.json()
+}).then(function(data){
+    console.log(data)
+}).catch((error) => console.log(error))
 
