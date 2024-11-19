06979f915627224f2c4859621ae49f582d4a5a2e
SMD OWAIS <111645920+SMD111-git@users.noreply.github.com>
2024-11-19 18:30:10 +0000

    object.js

---
+++ classes/object.js
@@ -0,0 +1,9 @@
+function multipleby5(num){
+    return num*5
+
+}
+multipleby5.power=2
+console.log(multipleby5(5));
+console.log(multipleby5.power);//the so imp thing is to note that the all the thing in in the js are at end realted to object and the object related to null
+//
+
