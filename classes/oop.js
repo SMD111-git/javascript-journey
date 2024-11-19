19c2670d6af8db52082a4dd8e96d98b951bfa06a
SMD OWAIS <111645920+SMD111-git@users.noreply.github.com>
2024-11-19 13:26:07 +0000

    oops

---
+++ classes/oop.js
@@ -0,0 +1,11 @@
+const user={
+    username:"SMD",
+    logincount:8,
+    signedin:true,
+    getuserdeatils:()=>{
+        console.log("got user details from database");
+        
+    }
+}
+console.log(user.username);
+console.log(user.getuserdeatils());
