b18a7ed9a111cdf88a4fec327f56078a3d24708e
SMD OWAIS <111645920+SMD111-git@users.noreply.github.com>
2024-11-19 16:51:51 +0000

    oops

--- classes/oop.js
+++ classes/oop.js
@@ -15,5 +15,26 @@ console.log(user.getuserdeatils());
 console.log(this);//this  excution in golbal reffernces as we get the output as empty , as there is  no current context as golbal reefernces 
 //to get the value but when u give it in the borwser it will give you "window" which having a list of values in it
 
-const promiseone = new Promise() 
+//const promiseone = new Promise() 
+/*constructor function  */
+//eg 
+function user(username,logincount,isloged){
+    this.username=username;//the key point is that "this" is used as reffernces of varaible or create an empty object to store the value in it
+    //so this.username  act as variable
+    this.logincount=logincount;
+    this.isloged=isloged;
 
+
+}
+
+const userone= user("hitesh",12,true);
+const usertwo=user("smd",11,false)
+console.log(userone);
+
+//as we see a varaible named userone is store init and the user function retirve it and ddipaly 
+//but as we give another varaible as usertwo thenn is  the output is  overwrited so to avoid this stuiation 
+//we use a keyword "new" which give rise to contructor function where we constrictor function is  
+//is a special type of function used to create and initialize objects. It is typically used to create multiple 
+//instances of a similar object type. Constructor functions are often written in a way that they are invoked using the new keyword, 
+//which ensures that a new object is created and the this keyword inside the constructor refers to that object
+//so  "new" keyword  gives a new instnances for every new varaible or object create.
