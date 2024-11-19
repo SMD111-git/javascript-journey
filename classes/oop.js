738d0d552dee31e104959b40cb53a5f6f5b7236d
SMD OWAIS <111645920+SMD111-git@users.noreply.github.com>
2024-11-19 14:15:15 +0000

    oops

--- classes/oop.js
+++ classes/oop.js
@@ -6,9 +6,13 @@ const user={
         console.log("got user details from database");
         //the keyword 'this ' refferes to the context to current  function or the class which need to  excuted
         console.log(`username:${this.username}`);
-        console.log(this);//as we it the current context values or reffernces 
+        console.log(this);//as we it the current context values .
                 
     }
 }
 console.log(user.username);
 console.log(user.getuserdeatils());
+console.log(this);//this  excution in golbal reffernces as we get the output as empty , as there is  no current context as golbal reefernces 
+//to get the value but when u give it in the borwser it will give you "window" which having a list of values in it
+
+
