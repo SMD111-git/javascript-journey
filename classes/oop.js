24ba01204ac780107a1ddba7d20e85c770cafe6c
SMD OWAIS <111645920+SMD111-git@users.noreply.github.com>
2024-11-19 13:59:52 +0000

    oops this

--- classes/oop.js
+++ classes/oop.js
@@ -4,11 +4,10 @@ const user={
     signedin:true,
     getuserdeatils:function(){
         console.log("got user details from database");
-        console.log(`username: ${this.username}`);
-        
+        //the keyword 'this ' refferes to the context to current  function or the class which need to  excuted
+        console.log(`username:${this.username}`);
         
     }
 }
 console.log(user.username);
 console.log(user.getuserdeatils());
- 
