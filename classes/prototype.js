0aab75f0affb7ce4ca39f9d165854b2e8f233df4
SMD OWAIS <111645920+SMD111-git@users.noreply.github.com>
2024-11-20 05:29:01 +0000

    oops

--- classes/prototype.js
+++ classes/prototype.js
@@ -29,4 +29,38 @@ power.smd()
 hero.md()
 //power.md()//as tthis is not a array
 
+// inheritance
 
+const User = {
+    name: "chai",
+    email: "chai@google.com"
+}
+
+const Teacher = {
+    makeVideo: true
+}
+
+const TeachingSupport = {
+    isAvailable: false
+}
+
+const TASupport = {
+    makeAssignment: 'JS assignment',
+    fullTime: true,
+    __proto__: TeachingSupport//here the teaching able  to  acess the tasupport values and methods
+}
+
+Teacher.__proto__ = User//here teacher is able to  acess the the user daya or values.
+
+// modern syntax
+Object.setPrototypeOf(TeachingSupport, Teacher)//teaching support can ascess the valuess or method of teacher.
+
+let anotherUsername = "ChaiAurCode     "
+String.prototype.trueLength = function(){
+    console.log(`${this}`);
+    console.log(`True length is: ${this.trim().length}`);
+}//the keyword  "this" points out the current value. 
+
+anotherUsername.trueLength()
+"hitesh".trueLength()
+"iceTea".trueLength()
